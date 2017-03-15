2ac7b55fd396ead56619d811ae30a9495dfebed8
USER-20161209JJ\Administrator <1044696905@qq.com>
2017-03-15 17:20:38 +0800

    PhotonServer(2017-3-15 输入日志)

---
+++ PhotonServer/MyGameServer/ClientPeer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+namespace MyGameServer
+{
+    class ClientPeer:Photon.SocketServer.ClientPeer
+    {
+        public ClientPeer(InitRequest initRequest):base(initRequest)
+        {
+
+        }
+
+        //处理客户端断开连接的后续工作
+        protected override void OnDisconnect(PhotonHostRuntimeInterfaces.DisconnectReason reasonCode, string reasonDetail)
+        {
+            
+        }
+
+        //处理客户端的请求
+        protected override void OnOperationRequest(Photon.SocketServer.OperationRequest operationRequest, Photon.SocketServer.SendParameters sendParameters)
+        {
+           
+        }
+    }
+}
---
+++ PhotonServer/MyGameServer/MyGameServer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using ExitGames.Logging;
+using System.IO;
+using ExitGames.Logging.Log4Net;
+using log4net.Config;
+
+namespace MyGameServer
+{
+    /// <summary>
+    /// 所有的Server端 主类都要继承自ApplicationBase
+    /// </summary>
+    public class MyGameServer:ApplicationBase
+    {
+        //定义一个日志对象用来输出日志
+        private static readonly ILogger log = LogManager.GetCurrentClassLogger();
+
+        //当一个客户端请求连接
+        //使用peerbase 表示和一个客户端的连接
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new ClientPeer(initRequest);
+        }
+
+        //初始化
+        protected override void Setup()
+        {
+            //配置日志所在的路径
+            log4net.GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(Path.Combine(this.ApplicationRootPath, "bin_Win64"),"log");
+            //日志的初始化
+            FileInfo configFileInfo = new FileInfo(Path.Combine(this.BinaryPath,"log4net.config"));
+            if (configFileInfo.Exists)
+            {
+                LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);//
+                XmlConfigurator.ConfigureAndWatch(configFileInfo);//让log4net这个插件读取配置文件
+            }
+            log.Info("Setup Completed!!!");
+            log.Info("王容发吃屎！！！！");
+        }
+
+        //Server端关闭的时候
+        protected override void TearDown()
+        {
+            
+        }
+    }
+}
